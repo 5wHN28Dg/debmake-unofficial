a720a6a45de0e593860063cb034d1eb45766d816
Osamu Aoki <osamu@debian.org>
2026-01-26 22:10:18 +0900

    src/debmake/* delete unused

--- src/debmake/data/desc_python3.short.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -e setup.py ]; then
-  python3 setup.py --description
-else
-  echo "Short description here"
-fi
--- src/debmake/data/desc_specin.long.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SPEC=$(ls -1 ./*.spec.in | head -n1)
-if [ -e $SPEC ]; then
-  sed -e '1,/^%description/d' $SPEC | sed -e '/^%/,$d'
-else
-  echo " Long description here"
-fi
