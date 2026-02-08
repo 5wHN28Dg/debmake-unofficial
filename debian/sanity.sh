c3aa557097aca2aa9c2a92a1b656269cb4f596ed
Osamu Aoki <osamu@debian.org>
2026-02-08 17:26:43 +0900

    NMU/test build safe
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- debian/sanity.sh
+++ debian/sanity.sh
@@ -8,14 +8,17 @@ for f in src/debmake/*.py ;
 # Checking hardcoded versions
 # debian/changelog
 DEBIAN_VERSION="$(dpkg-parsechangelog -S "Version")"
+# remove salsaci version bump/bin nmu version bump etc.
+echo "DEBIAN_VERSION = $DEBIAN_VERSION @ debian/changelog (original)"
+DEBIAN_VERSION="${DEBIAN_VERSION%%+*}"
 # src/debmake/__init__.py
+echo "DEBIAN_VERSION = $DEBIAN_VERSION @ debian/changelog (sanitized)"
 PYCODE_VERSION="$(sed -n -e '/^__version__/s/^__version__ = "\([^"][^"]*\)".*$/\1/p' src/debmake/__init__.py)"
+echo "PYCODE_VERSION = $PYCODE_VERSION @ src/debmake/__init__.py"
 if [ "$DEBIAN_VERSION" = "$PYCODE_VERSION" ]; then
   echo "All version OK: $DEBIAN_VERSION"
 else
   echo "XXX version mismatch XXX"
-  echo "DEBIAN_VERSION = $DEBIAN_VERSION @ debian/changelog"
-  echo "PYCODE_VERSION = $PYCODE_VERSION @ src/debmake/__init__.py"
   exit 1
 fi
 
